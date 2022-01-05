b7de76bf75d930e92827efb187a793eb7128a482
Daoud Clarke <daoud.clarke@gmail.com>
2022-01-05 23:16:19 +0000

    Use ES6 style classes; this.chooseDomain() is not working

--- src/background.js
+++ src/background.js
@@ -1,66 +1,72 @@
 chrome.runtime.onInstalled.addListener(() => {
-  Crawler.setUp();
+  run();
 });
 
 chrome.runtime.onStartup.addListener(() => {
-  Crawler.setUp();
+  run();
 });
 
 
-const Crawler = (() => {
-  var curatedDomains = [];
+function run() {
+  crawler = new Crawler();
+  crawler.setUp();
+}
 
-  return {
-    loadCuratedDomains: () => {
-      const url = chrome.runtime.getURL('../../assets/data/hn-top-domains.json');
-      fetch(url).then(response => {
-        response.json().then(data => {
-          curatedDomains = Object.keys(data);
-          console.log("Loaded curated domains", curatedDomains);
-        });
-      });
-    },
 
-    chooseDomain: () => {
-      time = new Date().getTime();
-      return curatedDomains[time % curatedDomains.length];
-    },
-
-    setUp: () => {
-      console.log("Starting up crawler extension");
-      Crawler.loadCuratedDomains();
-      setInterval(Crawler.runCrawlIteration, 1000);
-    },
-
-    runCrawlIteration: () => {
-      console.log("Running crawl iteration");
-
-      chrome.storage.local.get(["links"], storageResult => {
-        console.log("Got storage", storageResult);
-
-        var links;
-        if (Object.keys(storageResult).length == 0) {
-          links = ['https://' + Crawler.chooseDomain()];
-        } else {
-          links = storageResult.links;
-        }
-        console.log("Links", links);
-
-        const chosenLink = links[links.length - 1];
-        Crawler.crawlURL(chosenLink);
-      });
-    },
-
-    crawlURL: (url) => {
-      fetch(url).then(response => {
-        if (response.ok) {
-          response.text().then(responseText => {
-            console.log("Got response text", responseText);
-          })
-        } else {
-          console.log("Got bad response", response);
-        }
-      })
-    }
+class Crawler {
+  constructor() {
+    this.curatedDomains = [];
   }
-})();
+
+  loadCuratedDomains() {
+    const url = chrome.runtime.getURL('../../assets/data/hn-top-domains.json');
+    fetch(url).then(response => {
+      response.json().then(data => {
+        this.curatedDomains = Object.keys(data);
+        console.log("Loaded curated domains", this.curatedDomains);
+      });
+    });
+  }
+
+  chooseDomain() {
+    time = new Date().getTime();
+    return this.curatedDomains[time % this.curatedDomains.length];
+  }
+
+  setUp() {
+    console.log("Starting up crawler extension");
+    this.loadCuratedDomains();
+    setInterval(this.runCrawlIteration, 1000);
+  }
+
+  runCrawlIteration() {
+    console.log("Running crawl iteration");
+
+    chrome.storage.local.get(["links"], storageResult => {
+      console.log("Got storage", storageResult);
+
+      var links;
+      if (Object.keys(storageResult).length == 0) {
+        links = ['https://' + this.chooseDomain()];
+      } else {
+        links = storageResult.links;
+      }
+      console.log("Links", links);
+
+      const chosenLink = links[links.length - 1];
+      this.crawlURL(chosenLink);
+    });
+  }
+
+  crawlURL(url) {
+    fetch(url).then(response => {
+      if (response.ok) {
+        response.text().then(responseText => {
+          console.log("Got response text", responseText);
+        })
+      } else {
+        console.log("Got bad response", response);
+      }
+    });
+  }
+}
