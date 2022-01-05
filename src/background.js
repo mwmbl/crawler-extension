5f47db6c9ea060ca3085e4d9b0f003777105ef4f
Daoud Clarke <daoud.clarke@gmail.com>
2022-01-05 21:32:35 +0000

    Run a crawl iteration once every second  - Use manifest version 2 to be more compatible with Firefox  - We also need a persistent background script so we can run our task frequently  - Tried using alarms but the frequency was not able to control the frequency accurately

--- src/background.js
+++ src/background.js
@@ -2,5 +2,23 @@ let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log('Default background color set to %cbanana', `color: ${color}`);
+  setUp();
 });
+
+chrome.runtime.onStartup.addListener(() => {
+  setUp();
+});
+
+
+function setUp() {
+    console.log("Starting up crawler extension");
+//    chrome.alarms.onAlarm.addListener(runCrawlIteration);
+//    chrome.alarms.create("crawlAlarm", {delayInMinutes: 1.0/60, periodInMinutes: 0.1});
+    setInterval(runCrawlIteration, 1000);
+}
+
+
+function runCrawlIteration() {
+  console.log("Running crawl iteration");
+}
