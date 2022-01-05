515292d4aa17737343b718dc16b8a9bccf7bfb5d
Daoud Clarke <daoud.clarke@gmail.com>
2022-01-05 22:25:12 +0000

    Load curated set of domains and choose a random one at startup

--- src/background.js
+++ src/background.js
@@ -1,8 +1,4 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cbanana', `color: ${color}`);
   setUp();
 });
 
@@ -11,14 +7,49 @@ chrome.runtime.onStartup.addListener(() => {
 });
 
 
+const Crawler = (() => {
+  var curatedDomains = [];
+
+  return {
+    loadCuratedDomains: (() => {
+      const url = chrome.runtime.getURL('../../assets/data/hn-top-domains.json');
+      fetch(url).then(response => {
+        response.json().then(data => {
+          curatedDomains = Object.keys(data);
+          console.log("Loaded curated domains", curatedDomains);
+        });
+      });
+    }),
+
+    chooseDomain: (() => {
+      time = new Date().getTime();
+      return curatedDomains[time % curatedDomains.length];
+    })
+  }
+
+})();
+
 function setUp() {
     console.log("Starting up crawler extension");
-//    chrome.alarms.onAlarm.addListener(runCrawlIteration);
-//    chrome.alarms.create("crawlAlarm", {delayInMinutes: 1.0/60, periodInMinutes: 0.1});
+    Crawler.loadCuratedDomains();
     setInterval(runCrawlIteration, 1000);
 }
 
 
 function runCrawlIteration() {
   console.log("Running crawl iteration");
+
+  chrome.storage.local.get(["links"], storageResult => {
+    console.log("Got storage", storageResult);
+
+    var links;
+    if (Object.keys(storageResult).length == 0) {
+      links = ['https://' + Crawler.chooseDomain()];
+    } else {
+      links = storageResult.links;
+    }
+    console.log("Links", links);
+
+  });
+
 }
