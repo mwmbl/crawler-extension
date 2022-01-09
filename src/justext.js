fa26a78e3c8c016c6e3b5c48cba35cfcd2459daa
Daoud Clarke <daoud.clarke@gmail.com>
2022-01-09 22:02:55 +0000

    Classify paragraphs context free

--- src/justext.js
+++ src/justext.js
@@ -27,10 +27,29 @@ const PARAGRAPH_TAGS = new Set([
   'p', 'pre', 'table', 'td', 'textarea', 'tfoot', 'th', 'thead', 'tr',
   'ul', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
 ]);
+
+const HEADINGS = new Set(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']);
 const DEFAULT_ENCODING = 'utf8';
 const DEFAULT_ENC_ERRORS = 'replace';
 // const CHARSET_META_TAG_PATTERN = re.compile(br"""<meta[^>]+charset=["']?([^'"/>\s]+)""", re.IGNORECASE);
 const GOOD_OR_BAD = ['good', 'bad'];
+
+
+const STOPWORDS = new Set([
+  "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at",
+  "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could",
+  "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for",
+  "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
+  "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm",
+  "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't",
+  "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours",
+  "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so",
+  "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's",
+  "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until",
+  "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when",
+  "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would",
+  "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
+])
 
 
 class Paragraph {
@@ -39,7 +58,13 @@ class Paragraph {
     this.textNodes = [];
     this.charsCountInLinks = 0;
     this.tagsCount = 0;
+    this.cfClass = null;
     this.classType = "";
+    this.heading = this.domPath.some(x => HEADINGS.has(x));
+  }
+
+  getText() {
+    return this.textNodes.join(' ');
   }
 }
 
@@ -162,5 +187,43 @@ const makeParagraphs = (preprocessed, textNodeType) => {
 
 export const getParagraphs = (document, textNodeType) => {
   const preprocessed = preprocess(document);
-  return makeParagraphs(preprocessed, textNodeType);
+  const paragraphs = makeParagraphs(preprocessed, textNodeType);
+  classifyParagraphs(paragraphs);
+  return paragraphs;
 };
+
+
+export const classifyParagraphs = (paragraphs) => {
+  paragraphs.forEach(paragraph => {
+    const text = paragraph.getText();
+
+    const words = text.split(/\s+/);
+    const stopwordsCount = words.map(w => STOPWORDS.has(w.toLowerCase()) ? 1 : 0).reduce((x, y) => x + y, 0);
+    const stopwordDensity = words.length > 0 ? stopwordsCount / words.length : 0.0;
+    const linkDensity = text.length > 0 ? paragraph.charsCountInLinks / text.length : 0.0;
+
+    if (linkDensity > MAX_LINK_DENSITY_DEFAULT) {
+      paragraph.cfClass = 'bad';
+    } else if (text.search('\xa9') >= 0 || text.search('&copy') >= 0) {
+      // Copyright symbol
+      paragraph.cfClass = 'bad';
+    } else if (text.length < LENGTH_LOW_DEFAULT) {
+      if (paragraph.charsCountInLinks > 0) {
+        paragraph.cfClass = 'bad';
+      } else {
+        paragraph.cfClass = 'short';
+      }
+    } else if (stopwordDensity >= STOPWORDS_HIGH_DEFAULT) {
+      if (text.length > LENGTH_HIGH_DEFAULT) {
+        paragraph.cfClass = 'good';
+      } else {
+        paragraph.cfClass = 'neargood';
+      }
+    } else if (stopwordDensity >= STOPWORDS_LOW_DEFAULT) {
+      paragraph.cfClass = 'neargood';
+    } else {
+      paragraph.cfClass = 'bad';
+    }
+  });
+}
+
